429d7a45e4ed71514996c8d787f4ccc8b19b7b0c
Marianna Dei <d.marianna@icloud.com>
2022-12-11 12:39:20 +0200

    Unit conversion ℃|℉/update index.js

--- src/index.js
+++ src/index.js
@@ -79,4 +79,30 @@
   
   let currentLocationButton = document.querySelector("#current-location-button");
   currentLocationButton.addEventListener("click", getCurrentLocation);
+
+  function displayFahrenheitTemperature(event) {
+    event.preventDefault();
+    let temperatureElement = document.querySelector("#temperature");
+    
+    let fahrenheitTemperature = (celsiusTemperature * 9/5 + 32);
+    temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
+  }
+  
+  function displayCelsiusTemperature(event) {
+    event.preventDefault();
+  
+    let temperatureElement = document.querySelector("#temperature");
+    temperatureElement.innerHTML = Math.round(celsiusTemperature);
+  }
+  
+  let celsiusTemperature = null;
+  
+  let celsiusLink = document.querySelector("#celsius-link");
+  celsiusLink.addEventListener("click", displayCelsiusTemperature);
+  
+  let fahrenheitLink = document.querySelector("#fahrenheit-link");
+  fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
+  
+  searchCity("Kyiv");
+  
   
