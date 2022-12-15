0921ea48332cd44ae28ee264b0c803a5f9c6878c
Marianna Dei <d.marianna@icloud.com>
2022-12-15 23:41:09 +0200

    Forecast  API/update index.js

--- src/index.js
+++ src/index.js
@@ -50,6 +50,14 @@ function displayForecast(response) {
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
   
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "1dbf926d3b4417bf379db7043bec1047";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
+  
   function displayWeatherCondition(response) {
     let temperatureElement = document.querySelector("#temperature");
     let cityElement = document.querySelector("#city");
